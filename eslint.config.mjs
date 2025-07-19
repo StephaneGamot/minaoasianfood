c832f67d010d39ef4452d74ff832f70ad25b8a1e
stephane Gamot <stephanegamot@outlook.com>
2025-07-19 13:37:45 +0200

    prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Config de base pour Next.js et TypeScript
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ✅ Règle spécifique aux fichiers générés par Prisma
+  {
+    files: ["src/generated/prisma/*.js"],
+    languageOptions: {
+      sourceType: "commonjs", // facultatif mais explicite
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ];
 
 export default eslintConfig;

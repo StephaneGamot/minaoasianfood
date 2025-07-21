4728c9183503c306f652f27f7b436001883f80f4
stephane Gamot <stephanegamot@outlook.com>
2025-07-21 16:42:14 +0200

    21 juillet

--- src/app/[locale]/galerie/page.tsx
+++ src/app/[locale]/galerie/page.tsx
@@ -1,31 +1,22 @@
-import ResponsiveImage from "@/components/Gallery/ResponsiveImage";
-import Restaurant1 from "./../../../../public/images/restaurant1.jpg"
-import Restaurant2 from "./../../../../public/images/restaurant2.jpg"
-import { StaticImageData } from "next/image";
+import Gallery from "@/components/Gallery/Gallery";
+import img1 from "./../../../../public/images/restaurant1.jpg";
+import img2 from "./../../../../public/images/restaurant2.jpg";
+import img3 from "./../../../../public/images/restaurant2.jpg";
 
-type ResponsiveImageProps = {
-  src: string | StaticImageData; // <-- ici
-  alt: string;
-  rounded?: boolean;
-  priority?: boolean;
-  className?: string;
-};
+export default function GalleryPage() {
+  const images = [
+    { src: img1, alt: "Salle du restaurant" },
+    { src: img2, alt: "Tiramisu crémeux" },
+    { src: img3, alt: "Bo bun" },
+    // ajoute d’autres images ici
+  ];
 
-
-export default function Page() {
   return (
-    <div className="p-4 grid grid-cols-2 gap-4">
-      <ResponsiveImage
-        src={Restaurant1}
-        alt="Bœuf savoureux"
-        size="lg"
-      />
-      <ResponsiveImage
-       src={Restaurant2}
-        alt="Tiramisu crémeux"
-        size="md"
-        rounded={false}
-      />
-    </div>
+    <main aria-labelledby="gallery-heading" className="p-4">
+      <h1 className="text-3xl text-center font-bold text-gray-900 mb-4">
+        Gallerie de Photos
+      </h1>
+      <Gallery images={images} />
+    </main>
   );
 }

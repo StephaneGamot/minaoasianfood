543c56e25bc053a633e35a29dfe5c4a145f73031
stephane Gamot <stephanegamot@outlook.com>
2025-08-20 19:18:53 +0200

    test23

--- front/src/i18n/routing.ts
+++ front/src/i18n/routing.ts
@@ -1,7 +1,8 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['fr', 'en', 'nl'],
-  defaultLocale: 'fr',
+locales: ["fr", "en", "nl"] as const,
+  defaultLocale: "fr",
+  localePrefix: "always" as const
   
 });

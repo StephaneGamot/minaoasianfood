17f025eaf268e52aa856d4a61171b583108342fc
Stéphane Gamot <stephanegamot@outlook.com>
2025-12-03 16:13:57 +0100

    txt

--- front/src/components/Heros/HomePageHero.tsx
+++ front/src/components/Heros/HomePageHero.tsx
@@ -8,26 +8,19 @@ export default function HomePageHero() {
   const t = useTranslations('homePageHero');
 
   return (
-     <section className="relative overflow-hidden">
+     <section className="relative overflow-hidden !bg-[545454]">
       {/* Image de fond */}
-      <Image
-        src={ImgBGHero}
-        alt={'Delicious asian food'}
-        fill
-        priority
-        className="object-cover"
-      />
+  
 
-      {/* Filtre / overlay pour le contraste */}
-      <div className="absolute inset-0 bg-black/50" />
+  
 
       {/* Contenu */}
       <div className="relative text-center px-4 py-16 max-w-3xl mx-auto">
-        <h1 className="text-3xl md:text-4xl font-bold text-white mb-4">
+        <h1 className="!text-[#545454] text-3xl md:text-4xl font-bold  mb-4">
           {t('title')}
         </h1>
 
-        <p className="text-lg md:text-xl text-gray-100">
+        <p className="  text-lg md:text-xl !text-[#545454]">
           {t.rich('description', {
             strong: (chunks) => <strong className="font-semibold">{chunks}</strong>,
             span: (chunks) => (
